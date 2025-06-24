d7b1466119557e1cba115d76075d675a00b939ca
Bhanu2001829 <bhanuka.lakmal29@gmail.com>
2025-06-25 00:43:15 +0530

    New CV  added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -40,7 +40,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="https://drive.google.com/file/d/1RKwjC57jAPO0IkqV1RViWyJrxUQTz_Df/view?usp=sharing"
+                href="https://drive.google.com/file/d/16qPayUUd8CNgya8HCbLfc380G8FD9gCB/view?usp=sharing"
                 className="px-6 py-2.5 rounded-full border-2 border-primary text-primary hover:bg-primary/10 transition-all text-base sm:text-lg text-center hover:scale-105"
               >
                 Download CV
